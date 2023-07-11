4e3b343347df6a6d0864340b462d9f1e85ab5c91
YanaWallace <onecallnevada@gmail.com>
2023-07-11 08:45:12 -0700

    I have added Planet2 and it's working fine

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,12 @@ public class Main {
         YayaX.name="babe";
         YayaX.temp="89999888 celsius";
         YayaX.size="99999999km";
+
+        Planet2 millioner=new Planet2();
+        millioner.name="$9999999";
+        millioner.color="Blue";
+        millioner.temp="9898998 celsius";
+        millioner.size="999cm";
+
     }
 }
